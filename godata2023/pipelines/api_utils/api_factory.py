b1aff7e88db05f51ac29b04907f0a668cba86039
godata2023 <godata2023@outlook.com>
2025-03-26 10:10:15 +0800

    A1 requirement

--- godata2023/pipelines/api_utils/api_factory.py
+++ godata2023/pipelines/api_utils/api_factory.py
@@ -4,4 +4,26 @@ from datetime import datetime
 import configparser
 
 class APIHandler:
-    pass
+    def __init__(
+            self,
+            request_params,
+            base_url
+    ):
+        self.base_url = base_url
+        self.request_params = request_params
+
+    def read_api_key(self) -> str:
+        config = configparser.ConfigParser()
+        config.read(
+            '/Users/xxx/xxx/xxx/xxx/xxx/xxx/godata2023/config/config.ini' # use absolute path
+        )  # Path to your configuration file
+        api_key = config.get('API', 'api_key')
+        return api_key
+    
+    def get_endpoint(self):
+        #TODO:
+        return url
+
+    def request_date(self, url):
+        #TODO:
+        return data
