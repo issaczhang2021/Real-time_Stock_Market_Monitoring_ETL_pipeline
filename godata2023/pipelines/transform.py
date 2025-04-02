f1cdf141be30b4c49c3498b62c962528e972604e
godata2023 <godata2023@outlook.com>
2025-04-01 21:40:53 -0400

    add code during class3

--- godata2023/pipelines/transform.py
+++ godata2023/pipelines/transform.py
@@ -14,4 +14,34 @@ def stock_events_normalizer(data: dict, spark: SparkSession):
     """
     take stock events and process into df
     """
-    pass
+    symbol = data["Meta Data"].get('2. Symbol', 'N/A')
+
+    # Extract time series data, 2nd element of the dict
+    body = data.get(list(data.keys())[1])
+
+    enrich_events = []
+    for date, dict_value in body.items():
+        dict_value['symbol'] = dict_value.get('symbol', symbol)
+        dict_value['trade_timestamp'] = dict_value.get('trade_timestamp', date)
+        enrich_events.append(dict_value)
+
+    df_raw = spark.createDataFrame(enrich_events)
+    # type casting
+    df = df_raw.select(
+        df_raw["trade_timestamp"]
+        .cast(TimestampType())
+        .alias("trade_timestamp"),
+        df_raw["symbol"].cast(StringType()).alias("symbol"),
+        df_raw["`1. open`"].cast(DoubleType()).alias("open"),
+        df_raw["`2. high`"].cast(DoubleType()).alias("high"),
+        df_raw["`3. low`"].cast(DoubleType()).alias("low"),
+        df_raw["`4. close`"].cast(DoubleType()).alias("close"),
+        df_raw["`5. volume`"].cast(IntegerType()).alias("volume"),
+    )
+
+    batch_id = current_timestamp().cast("long")
+    df = df.withColumn("partition", to_date("trade_timestamp").cast("string"))
+    df = df.withColumn("batch_id", lit(batch_id))
+    # Show the DataFrame for testing
+    # df.show()
+    return df
